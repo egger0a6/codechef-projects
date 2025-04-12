5339f99988efa735c5bd92b7c4052b7edc9ebfcf
Zach Eggert <egger0a6@email.arizona.edu>
2025-04-12 18:48:03 -0400

    word_counter.py: add functions to split input text using regex, to get user input for a word to search and return the frequency of that word in the text, and to display the results

--- python/word_counter.py
+++ python/word_counter.py
@@ -1,5 +1,6 @@
-def read_input_file(filename):
+import re
 
+def read_input_file(filename):
     try:
         # Open the file in read mode ('r')
         with open(filename, "r") as file:
@@ -11,10 +12,36 @@ def read_input_file(filename):
         print(f"Error: The file '{filename}' was not found.")
         # Return an empty string if the file is not found
         return ""
-    
+
+
+def get_words_split(text):
+    # Split the text into words
+    words = text.split()
+    return words
+
+
+def get_words_regex(text):
+    # Use regex to extract words while ignoring punctuation  
+    words = re.findall(r"\b\w+\b", text)
+    return words
+
+def word_frequency(words):
+    search_word = input("Enter the word to search: ")
+    word_freq = words.count(search_word)
+    return word_freq, search_word
+
+
+def display_results(total_words, search_word, word_frequency):
+    print(f"Total Words: {total_words}")
+    print(f"Frequency of '{search_word}': {word_frequency}")
+
 
 if __name__ == "__main__":
-    
-    filename = 'input.txt'
+    filename = 'text.txt'
     text = read_input_file(filename)
-    print(text)
+    
+    if text:
+        words = get_words_regex(text)
+        total_words = len(words)
+        word_freq, search_word = word_frequency(words)
+        display_results(total_words, search_word, word_freq)
