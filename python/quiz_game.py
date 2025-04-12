7ca353ca72af46e0733135a0f506ae227f97888a
Zach Eggert <egger0a6@email.arizona.edu>
2025-04-12 02:39:09 -0400

    quiz_game.py: add check_answer func to check user answer against the correct answer, add update_score func to update score, add a few more questons

--- python/quiz_game.py
+++ python/quiz_game.py
@@ -14,10 +14,28 @@ def get_questions():
             "question": "What is the output of print(2 ** 3) in Python?", 
             "options": ["A) 6", "B) 8", "C) 9", "D) 10"], 
             "answer": "B"
-            },
+        },
 
         {
-            "question" "Which of the following operators has the highest precedence?",
+            "question": "Which data type is mutable in Python?", 
+            "options": ["A) Tuple", "B) String", "C) List", "D) Integer"], 
+            "answer": "C"
+        },
+        
+        {
+            "question": "Which of the following is used to take input from the user?", 
+            "options": ["A) input()", "B) print()", "C) scan()", "D) read()"], 
+            "answer": "A"
+        },
+        
+        {
+            "question": "What will be the output of len('Python')?",
+            "options": ["A) 5", "B) 6", "C) 7", "D) Error"], 
+            "answer": "B"
+        }
+
+        {
+            "question": "Which of the following operators has the highest precedence?",
             "options": ["A) % (Modulus)", "B) & (BitWise AND)", "C) ** (Exponent)", "D) > (Comparison)"],
             "answer": "C"
         },
@@ -74,9 +92,8 @@ def countdown_timer():
 
 
 def display_question(question_data, question_number):
-    # -------------------- WRITE YOUR CODE HERE ----------------------
     print("\n====================")
-    print(f"Question {question_number}: {question_data['question']}")
+    print(f"Question {question_number}: {question_data['question']}\n")
     for option in question_data["options"]:
         print(option)
 
@@ -86,7 +103,22 @@ def get_user_answer():
     if user_answer in ["A", "B", "C", "D"]:
         return user_answer
     else:
-        return None        
+        return None
+
+
+def check_answer(user_answer, correct_answer):
+    if user_answer == correct_answer:
+        print("Answer is correct!")
+        return 1
+    else:
+        print(f"Your answer is wrong! The correct answer is {correct_answer}.")
+        return 0
+
+
+def update_score(score, result):
+    if result:
+        score += 1
+    return score
 
 
 def play_quiz():
@@ -101,7 +133,11 @@ def play_quiz():
         display_question(question, i)
         countdown_timer()
         user_answer = get_user_answer()
-        print("your response: ", user_answer)
+        result = check_answer(user_answer, question["answer"])
+        score = update_score(score, result)
+
+    print(f"Your final score is: {score} / {len(questions)}")
+    print("Thanks for playing!")
 
 # --------------------------------------------------   
 
