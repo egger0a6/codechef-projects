5278ef07c02450b538f383236f4d6c4e2f898c2f
Zach Eggert <egger0a6@email.arizona.edu>
2025-05-19 01:22:42 -0400

    rock_paper_scissors: add functions to get computer choice, determine the winner of a round, and determine the winner of the game. Update main so user selects how many rounds will be played

--- python/rock_paper_scissors.py
+++ python/rock_paper_scissors.py
@@ -3,7 +3,7 @@ import random
 you_score = 0
 computer_score = 0
 
-# Update the get_user_choice() function
+
 def get_user_choice():
     """
     Function to get user's choice (rock, paper, or scissors)
@@ -15,17 +15,50 @@ def get_user_choice():
         else:
             print("Invalid choice! Please enter 'rock', 'paper', or 'scissors'.")
 
+def get_computer_choice():
+    """
+    Function to randomly generate computer's choice
+    """
+    return random.choice(['rock', 'paper', 'scissors'])
 
-"""
-Main code
-"""
+def determine_winner(user_choice, computer_choice):
+    """
+    Function to determine the winner of the game
+    """
+    global you_score, computer_score
+
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "paper") or (user_choice == "paper" and computer_choice == "scissors") or (user_choice == "scissors" and computer_choice == "rock"):
+        computer_score += 1
+        return "Computer wins this round!"
+    else:
+        you_score += 1
+        return "Congratulations! You win this round!"
+    
+def overall_winner(you_score, computer_score):
+    if you_score < computer_score:
+        return("Computer won the overall match")
+    elif you_score > computer_score:
+        return("You won the overall match")
+    else:
+        return("The match is a tie")
+
+
 if __name__ == '__main__':
     print("Let's play Rock, Paper, Scissors!")
     print("This game is the best of 3!")
     print("\n")
+    n = int(input("How many rounds do you want to play? "))
     
-    n = 0
-    while n < 3:
-        print(f"Round: {n+1}")
+    while n > 0:
+        print(f"Round: {n}")
         user_choice = get_user_choice()
-        n = n + 1
+        computer_choice = get_computer_choice()
+        print(f"You chose: {user_choice}")
+        print(f"Computer chose: {computer_choice}")
+        print(determine_winner(user_choice, computer_choice))
+        n = n - 1
+        
+    print("\n")
+    print(overall_winner(you_score, computer_score))
